1488daf38f1f7de15b035bc44bd0a1da45540241
Maurice Royce <mauriceroyce@flames.lan>
2016-10-04 13:35:06 -0700

    fix gen_dev_data

--- momo_api/account/tests/helpers.py
+++ momo_api/account/tests/helpers.py
@@ -33,31 +33,49 @@ def create_test_user_profile(username='test user', password='test', first_name='
     Create and return a test `UserProfile` object
     """
     # create an account for this profile
-    create_test_account(username, password)
+    account_create_kwargs = {
+        'account_type': Account.USER,
+    }
+    account = create_test_account(username, password, **account_create_kwargs)
 
-    avatar = create_test_image()
-    create_kwargs = {
-        'avatar': avatar,
+    # update the user profile associated with this account
+    user_profile = account.profile
+
+    profile_update_kwargs = {
+        'avatar': create_test_image(),
         'first_name': first_name,
         'last_name': last_name,
     }
+    profile_update_kwargs.update(kwargs)
 
-    create_kwargs.update(kwargs)
-    return UserProfile.objects.create(**create_kwargs)
+    for attr, value in profile_update_kwargs.items():
+        setattr(user_profile, attr, value)
+    user_profile.save()
+
+    return user_profile
 
 
 def create_test_company_profile(username='test company', password='test', name='test company', **kwargs):
     """
-    Create and return a test CompanyProfile object
+    Create and return a test `CompanyProfile` object
     """
     # create an account for this profile
-    create_test_account(username, password)
+    account_create_kwargs = {
+        'account_type': Account.COMPANY,
+    }
+    account = create_test_account(username, password, **account_create_kwargs)
 
-    avatar = create_test_image()
-    create_kwargs = {
-        'avatar': avatar,
+    # update the company profile associated with this account
+    company_profile = account.profile
+
+    profile_update_kwargs = {
+        'avatar': create_test_image(),
         'name': name,
     }
+    profile_update_kwargs.update(kwargs)
 
-    create_kwargs.update(kwargs)
-    return CompanyProfile.objects.create(**create_kwargs)
+    for attr, value in profile_update_kwargs.items():
+        setattr(company_profile, attr, value)
+    company_profile.save()
+
+    return company_profile
