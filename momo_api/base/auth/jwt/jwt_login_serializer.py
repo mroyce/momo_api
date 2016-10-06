d3ec07e38c7892a156995e4a77b35c959e65afb9
Maurice Royce <mauriceroyce@flames.lan>
2016-10-06 09:54:55 -0700

    base migrations

--- momo_api/base/auth/jwt/jwt_login_serializer.py
+++ momo_api/base/auth/jwt/jwt_login_serializer.py
@@ -1,7 +1,8 @@
-from django.conf import settings
 from django.contrib.auth import authenticate
 
 from rest_framework import serializers
+
+from momo_api.account.models import Account
 
 from .jwt_authentication import create_jwt_token
 
@@ -15,8 +16,8 @@ class JWTLoginSerializer(serializers.Serializer):
     Adapted from:
     https://github.com/GetBlimp/django-rest-framework-jwt/blob/master/rest_framework_jwt/serializers.py
     """
-    username = serializers.CharField(max_length=settings.AUTH_USER_MODEL._meta.get_field('username').max_length)
-    password = serializers.CharField(write_only=True, max_length=settings.AUTH_USER_MODEL._meta.get_field('password').max_length)
+    username = serializers.CharField(max_length=Account._meta.get_field('username').max_length)
+    password = serializers.CharField(write_only=True, max_length=Account._meta.get_field('password').max_length)
 
     MISSING_CREDENTIALS = 'Must include "username" and "password".'
     INVALID_CREDENTIALS = 'Unable to login with the provided credentials.'
